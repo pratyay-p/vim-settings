99dfe2d469cdd846318991eeaf5eab032bfc4a35
pratyaypande <pratyaypande@gmail.com>
2021-03-19 07:46:00 +0530

    Added additional colors and starter code

--- codelines/cpp_small_main.cpp
+++ codelines/cpp_small_main.cpp
@@ -2,43 +2,64 @@
 #define lli long long int
 #define ull unsigned long long
 using namespace std;
+const int INF = 10000;
+const int MX = 100000;
+const int MOD = 1000000009;
 template<typename A,typename B>
-inline A fast_power(A x,B y){if(y<=1)return x;A k=fast_power(x,y/2);k=k*k;if(y%2==1)k=k*x;return k;}
+lli fpow(A x,B y,long M)
+{
+    if(y == 1) return x%M;
+    if(y == 0) return 1;
+    A k = fpow(x, y/2, M);
+    if(y%2 == 0)
+        return (k * k) % M;
+    return (((k * k) % M) * x) % M;
+}
 template<typename A>
 inline int gcd(A a,A b){A t;while((a!=0)&&(b!=0)){t=a%b;a=b;b=t;}return a+b;}
 template<typename A,typename B>
-inline lli ncr(A x,B r){lli top=1,bot=1;while(r>=1){top*=(x-r+1);bot*=r;r--;} return top/bot;}
-template<typename A>
-inline pair<A,int> kadane(A x[], int n) {
-    A max_so_far = -9999999999999, max_ending_here = 0; int h = 0;
-    for(int i=0;i<n;i++) { max_ending_here += x[i];
-        if(max_so_far < max_ending_here) { max_so_far = max_ending_here; h = i; }
-        if(max_ending_here < 0) max_ending_here = 0;}
-    return make_pair(max_so_far,h);
+inline long modular_inverse(A n,B M) { return fpow<A,B>(n,M-2,(long)M); }
+template<typename A,typename B>
+inline lli ncr(A x,B r,long M) {
+    if(r == 0) return 1; lli v = ncr(x-1,r-1,M);
+    v = (x*v)%M; v = (x*modular_inverse<A,B>(r,M))%M; return v;
 }
 struct custom_hash
 {
     template <class T1, class T2>
     size_t operator()(const pair<T1, T2>& p) const
-    { auto hash1 = hash<T1>{}(p.first); auto hash2 = hash<T2>{}(p.second); return hash1 ^ hash2; }
-    template <typename T1>
-    size_t operator()(const T1& p) const
-    { auto hash1 = hash<T1>{}(p); return hash1;
-        /* auto hash2 = hash<T1>{}(p.first); return hash1^hash2; */ }
+    {
+        auto hash1=hash<T1>{}(p.first);auto hash2=hash<T2>{}(p.second);
+        return hash1^hash2;
+    }
 };
-const int MX = 100000;
-int a[MX];
-void solve()
+int mxkq=0;
+inline void buildSieve(int xw,int sieve[]) 
 {
-
+    for(int i=mxkq;i<=max(xw,mxkq);i++)
+        sieve[i] = 0;
+    for(int i = max(2,mxkq); i <= xw;i++)
+    {
+        if(sieve[i])
+            continue;
+        for(int j = 2*i;j <= xw;j += i)
+            sieve[j] = i;
+    }
+    mxkq = max(mxkq,xw);
 }
+auto cmp = [](pair<int,int> left, pair<int,int> right)
+{
+    return(left.second < right.second);
+};
+int a[MX];
+int sieve[MX+1];
 int main()
 {
-    ios_base::sync_with_stdio(0);
-    cin.tie(0);
-    int t = 1;
+    ios_base::sync_with_stdio(0); cin.tie(0); int t = 1;
     //cin >> t;
     while(t--)
-        solve();
+    {
+
+    }
     return 0;
 }
---
+++ codelines/python_beginning_main.py
@@ -0,0 +1,9 @@
+import numpy as np
+import pandas as pd
+def solve():
+    n = int(input())
+
+
+t = int(input())
+for(i from 1 to n):
+    solve()
