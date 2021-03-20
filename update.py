dac3ec94c98c6564c0dad611f0d72568cea7b075
pratyaypande <pratyaypande@gmail.com>
2021-03-20 11:42:46 +0530

    Commit from update.py
    Commit made on: 03/20/2021, 11:42:46 IST

--- update.py
+++ update.py
@@ -1,14 +1,14 @@
 import subprocess
 from datetime import datetime
+import time
 print('Pull(d) or push(u)?')
 n = str(input())
 if n == 'd':
     subprocess.run(['git','pull'],check=True)
 if n == 'u':
-    nw = datetime.now()
-    msg = nw.strftime("%m/%d/%Y, %H:%M:%S")
+    msg = datetime.now().strftime("Commit made on: %m/%d/%Y, %H:%M:%S ") + time.tzname[time.daylight]
     subprocess.run(['git','add','.'],check=True)
-    subprocess.run(['git','commit','-m','Commit from script','-m',msg],check=True)
+    subprocess.run(['git','commit','-m','Commit from update.py','-m',msg],check=True)
     subprocess.run(['git','push','origin','master'],check=True)
 else:
     print('Invalid choice. Execute this again')
