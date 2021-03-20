520de79d5c63d95aa3955761e9405bc2f5077f36
pratyaypande <pratyaypande@gmail.com>
2021-03-20 11:23:45 +0530

    Script for updationw

---
+++ update.py
@@ -0,0 +1,14 @@
+import subprocess
+import datetime import datetime
+print('Pull(d) or push(u)?')
+n = str(input())
+if n == 'd':
+    subprocess.run(['git','pull'],check=True)
+else if n == 'u':
+    nw = datetime.now()
+    msg = nw.strftime("%m/%d/%Y, %H:%M:%S")
+    subprocess.run(['git','add','.'],check=True)
+    subprocess.run(['git','commit','-m','Commit from script','-m',msg],check=True)
+    subprocess.run(['git','push','origin','master'],check=True)
+else:
+    print('Invalid choice. Execute this again')
