5043b924dcc9d126ff5792dc1feb9d9ab7eb013f
Pratyay Pande <pratyaypande@gmail.com>
2021-05-12 07:52:05 +0530

    Updated installation script

--- install.py
+++ install.py
@@ -11,17 +11,17 @@ import time
 nsec = 7
 print('Need to download some plugins and copy some files\nRough size of download at the time of writing this is 160 MB some time will be needed for compilation\n\nPress Ctrl-c to terminate the download\n\n')
 
-for i from 1 to nsec:
-    time.sleep(1);
-print('Starting download...\nA suggestion would be to let this run while you work.\nMake sure you have a stable internet connection\n\n')
-subprocess.run(['sudo','apt','update','-y'],check=True)
-subprocess.run(['sudo','apt','upgrade','-y'],check=True)
-subprocess.run(['sudo','apt','remove','vim-tiny','-y'], check=True)
-subprocess.run(['sudo','apt','autoremove'],check=True)
-subprocess.run(['mv','~/.vim','vim-folder-backup'],check=True)
-subprocess.run(['mv','~/.vimrc','backup-vimrc.vim'],check=True)
-subprocess.run(['mv','vim-settings','~/.vim'],check=True)
-subprocess.run(['echo','source ~/.vim/myvimrc.vim','>>','~/.vimrc'],check=True)
+#for i in range(nsec):
+#    time.sleep(1);
+#print('Starting download...\nA suggestion would be to let this run while you work.\nMake sure you have a stable internet connection\n\n')
+#subprocess.run(['sudo','apt','update','-y'],check=True)
+#subprocess.run(['sudo','apt','upgrade','-y'],check=True)
+#subprocess.run(['sudo','apt','remove','vim-tiny','-y'], check=True)
+#subprocess.run(['sudo','apt','autoremove'],check=True)
+#subprocess.run(['mv','~/.vim','~/vim-folder-backup'],check=True)
+#subprocess.run(['mv','~/.vimrc','~/backup-vimrc.vim'],check=True)
+#subprocess.run(['mv','~/vim-settings','~/.vim'],check=True)
+#subprocess.run(['echo','source ~/.vim/myvimrc.vim','>>','~/.vimrc'],check=True)
 subprocess.run(['sudo','apt','install', '-y', 'vim-gtk3','build-essential','cmake', 'vim-nox', 'python3-dev'])
 subprocess.run(['sudo','apt','install', '-y', 'mono-complete', 'golang', 'nodejs', 'default-jdk', 'npm'])
 subprocess.run(['git','clone','git@github.com:VundleVim/Vundle.vim.git','~/.vim/bundle/Vundle.vim'])
