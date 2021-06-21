e0e920031b830ce74371faaea76d78df20618ddc
Pratyay Pande <pratyaypande.gmail.com>
2021-06-21 23:53:59 +0530

    C starter code

---
+++ codelines/c_main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <math.h>
+
+#define MX 100000
+#define MOD 1000000009
+
+bool isPowerOf2(int f) { return(f && !(f & (f - 1)));}
+char getC() { char f = getchar(); return(f); }
+void putC(char c) { putchar(c); }
+void getMod(int *v) { *v = (*v%10); }
+int main()
+{
+
+    return(0);
+}
